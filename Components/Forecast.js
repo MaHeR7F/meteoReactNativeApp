84e3cf588e2526309514a6ea42c6e0d44b02352e
Maher <maher.fettah@gmail.com>
2023-03-09 10:40:26 +0100

    debug flatlist

--- Components/Forecast.js
+++ Components/Forecast.js
@@ -1,39 +1,142 @@
 import React from 'react';
-import { FlatList, Image, Text, View } from 'react-native';
+import {FlatList, Image, StyleSheet, Text, View} from 'react-native';
 
-export default function Forecast({ icon, forecast, styles }) {
-    const renderItem = ({ item }) => {
-        const date = new Date(item.dt * 1000);
-        const dayOfWeek = date.toLocaleDateString('fr-FR', { weekday: 'short' });
-        const time = date.toLocaleTimeString('en-US', {
-            hour: 'numeric',
-            minute: 'numeric',
-            hour12: true,
-        });
 
-        return (
-            <View style={ styles.item }>
-                <Text style={ styles.time }>{ `${dayOfWeek} ${time}` }</Text>
-                <View style={ styles.iconContainer }>
-                    <Image style={ styles.icon } source={ icon } />
-                </View>
-                <Text>{ item.weather[0].description }</Text>
-                <Text style={ styles.temp }>{ Math.round(item.main.temp) }°C</Text>
-            </View>
-        );
-    };
+/*const renderItem = ({item}) => {
+    const backgroundColor = item.id === selectedId ? '#6e3b6e' : '#f9c2ff';
+    const color = item.id === selectedId ? 'white' : 'black';
 
     return (
-        <View style={ styles.nextContainer }>
-            <View style={ styles.titleNextContainer }>
+        <Item
+            item={item}
+            onPress={() => setSelectedId(item.id)}
+            backgroundColor={backgroundColor}
+            textColor={color}
+        />
+    );
+};*/
+const renderItem = (item) => {
+
+    const date = new Date(item.item.dt * 1000);
+    const dayOfWeek = date.toLocaleDateString('fr-FR', {weekday: 'short'});
+    const time = date.toLocaleTimeString('en-US', {
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: true,
+    });
+    console.log('item')
+console.log(item)
+    return (
+            <View style={styles.item}>
+                <Text style={styles.time}>{`${dayOfWeek} ${time}`}</Text>
+              <View style={styles.iconContainer}>
+                    <Image style={styles.icon} source={{uri :`http://openweathermap.org/img/wn/${item.item.weather && item.item.weather[0].icon}@4x.png`}}/>
+                </View>
+                <Text>{item.item.weather[0].description}</Text>
+               <Text style={styles.temp}>{Math.round(item.item.main.temp)}°C</Text>
+            </View>
+    )
+}
+export default function Forecast({forecast}) {
+
+
+        return (
+        <View style={styles.nextContainer}>
+            <View style={styles.titleNextContainer}>
                 <Text>Et la météo des jours suivants:</Text>
             </View>
-            <FlatList
-                data={ forecast }
-                renderItem={ renderItem }
-                keyExtractor={ (item, index) => index.toString() }
-                //horizontal={ true }
-                style={styles.flatList}/>
+            <View>
+                <FlatList
+                    data={forecast}
+                    keyExtractor={(item) => item.toString()}
+                    horizontal={true}
+
+                    renderItem={renderItem}
+                />
+            </View>
         </View>
-    );
+    )
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        height: '100%'
+    },
+    currentWeatherContainer:{
+        flexDirection:'column',
+        marginTop: '20%',
+        paddingTop:'20%',
+        width: '95%',
+        backgroundColor: '#FEFEE2',
+        borderWidth: 1,
+        padding: 10,
+    },
+    textIntro:{
+        padding: 20,
+        textAlign: "center"
+    },
+    text: {
+        textAlign: "center"
+    },
+    icon:{
+        height:100,
+        width:100,
+        borderWidth: 1,
+        backgroundColor: '#FFFFFF'
+    },
+    textContainer: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    cityText:{
+        fontSize:24,
+        textAlign: "center"
+    },
+    tempText:{
+        fontSize:20,
+        paddingTop: 10,
+        textAlign: "center"
+    },
+    iconContainer: {
+        alignItems: 'center'
+    },
+    forecastContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        margin: 20,
+        borderWidth: 1,
+        paddingTop: 10,
+        paddingBottom: 10
+    },
+    item: {
+        alignItems: 'center',
+        paddingHorizontal: 10,
+        paddingBottom:10
+    },
+    time: {
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    temp: {
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    scrollview: {
+        flex: 1
+    },
+    flatList: {
+        flex: 1,
+        width: '100%'
+    },
+    nextContainer: {
+        alignItems: 'center',
+        marginTop: 30
+    },
+    titleNextContainer: {
+        padding: 10,
+        textAlign: "center"
+    }
+});
